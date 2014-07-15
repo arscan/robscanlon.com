b12686a3e6255b1a9eb8d454372946898d30c7b3
Rob Scanlon <robscanlon@gmail.com>
2014-07-14 21:53:10 -0400

    kq progress

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -90,3 +90,98 @@ var s = skrollr.init();
 
 skrollr.menu.init(s);
 
+/* kings quest */
+
+var Entity = Object.create(null, {
+    location: {value: {top: 0, left: 0,  width: 0, height: 0}, enumerable: true, writable: true},
+    bounds: {value: {top: 0, left: 0, width: 0, height: 0}, enumerable: true, writable: true},
+    image: {value: null, enumerable: true, writable: true},
+    element: {value: null, writable: true},
+    parent: {value: null, writable: true},
+
+    collidesWith: {value: function(other){
+
+        return (other.bounds.top < this.bounds.top + this.bounds.height
+                && other.bounds.top + other.bounds.height > this.bounds.top
+                && other.bounds.left < this.bounds.left + this.bounds.width 
+                && other.bounds.left + other.bounds.width > this.bounds.left);
+    }},
+    render: {value: function(){
+
+        if(this.parent === null){
+            return null;
+        }
+
+        if(!this.element) {
+            /* TODO: FIGURE OUT WHY THIS ISN'T WORKING */
+            this.element = $('div');
+            this.parent.append(this.element);
+        }
+
+        this.element.css({
+            "background-image": this.image,
+            width: this.width,
+            height: this.height,
+            "z-index" : 1000 - this.height
+        });
+    }}
+});
+
+
+var Sprite = Object.create(Entity,{
+    frameCount: {value: 0, writable: true},
+    cols: { value: 1, writable: true},
+    currentFrame: {value: 0, writable: true},
+    facingDown: {value: true, writable: true},
+    facingLeft: {value: true, writable: true},
+
+    animate: {value: function(){
+        this.currentFrame++;
+        console.log(this.currentFrame);
+    }}
+});
+
+var realm = {
+    statics: [],
+    placeStatics: function(config){
+
+        for(var i = 0; i< config.length; i++){
+            var tmp = Object.create(Entity);
+
+            tmp.parent = $('#kq-game');
+            tmp.image = '../images/kq/staticobject' + i + '.png';
+            tmp.location = {
+                top: config[i][0],
+                left: config[i][1],
+                width: config[i][2],
+                height: config[i][3]
+            }
+            // TODO: FIGURE OUT WHY THIS ISN'T WORKING
+            //tmp.render();
+
+            this.statics.push(tmp);
+        }
+    }
+};
+
+ var graham = Object.create(Sprite);
+
+ $(function(){
+ realm.placeStatics([
+     [0,0,138,250],
+     [112,50,100,92],
+     [129,142,100,36],
+     [212,9,131,155],
+     [320,70,94,135],
+     [441,88,66,120],
+     [829,47,130,164],
+     [728,83,99,131],
+     [942,27,123,127],
+     [985,167,32,23],
+     [1097,34,144,184],
+     [1056,56,115,115]]);
+  
+  setInterval(function(){graham.animate()}, 1000);
+
+ });
+
